cc561821411d0ed333f640b3c62987850a1b7b09
Jas Randhawa <jrandhawa56@my.bcit.ca>
2022-11-27 20:16:05 -0800

    Update shopkeeper.py
    Convert dictionary values from tuples into nested dictionary

--- shopkeeper.py
+++ shopkeeper.py
@@ -3,26 +3,90 @@ from get_user_choice import get_user_choice
 
 def shopkeeper(player):
     weapons_dictionary = {
-        "The President's Coffee Mug": (1, 1, 1),
-        'Republican Hat': (1, 1, 1),
-        'Democrat Hat': (1, 1, 1),
-        'Bible': (1, 5, 5),
-        'Unfinished Memo to Justin Trudeau': (2, 6, 8),
-        'Presidential Pen': (2, 10, 12),
-        'Painting of Abraham Lincoln': (2, 12, 15),
-        'Seized Documents from Mar-a-Lago': (2, 15, 22),
-        'Bald Eagle': (3, 20, 50),
-        'The Constitution': (3, 40, 80)
+        "The President's Coffee Mug": {
+            'User Level Required': 1,
+            'Weapon Cost': 1,
+            'Attack Gain': 1
+        },
+        'Republican Hat': {
+            'User Level Required': 1,
+            'Weapon Cost': 1,
+            'Attack Gain': 1
+        },
+        'Democrat Hat': {
+            'User Level Required': 1,
+            'Weapon Cost': 1,
+            'Attack Gain': 1
+        },
+        'Bible': {
+            'User Level Required': 1,
+            'Weapon Cost': 5,
+            'Attack Gain': 5
+        },
+        'Unfinished Memo to Justin Trudeau': {
+            'User Level Required': 2,
+            'Weapon Cost': 6,
+            'Attack Gain': 8
+        },
+        'Presidential Pen': {
+            'User Level Required': 2,
+            'Weapon Cost': 10,
+            'Attack Gain': 12
+        },
+        'Painting of Abraham Lincoln': {
+            'User Level Required': 2,
+            'Weapon Cost': 12,
+            'Attack Gain': 15
+        },
+        'Seized Documents from Mar-a-Lago': {
+            'User Level Required': 2,
+            'Weapon Cost': 15,
+            'Attack Gain': 22
+        },
+        'Bald Eagle': {
+            'User Level Required': 3,
+            'Weapon Cost': 20,
+            'Attack Gain': 50
+        },
+        'The Constitution': {
+            'User Level Required': 3,
+            'Weapon Cost': 40,
+            'Attack Gain': 80
+        },
     }
     food_dictionary = {
-        "Bourbon": (1, 2),
-        "Turkey": (2, 4),
-        "Chicago-style Pizza": (3, 6),
-        "Cheeseburger": (4, 7),
-        "Philly Cheese Steak": (5, 10),
-        "Nashville Hot Chicken": (8, 20),
-        "Melania's Homemade Dinner from 2019": (15, 35),
-        "Michelle's Totally-Not-Gone-Bad Breakfast from 2016": (25, 100)
+        "Bourbon": {
+            'Food Cost': 1,
+            'Health Recovered': 2
+        },
+        "Turkey": {
+            'Food Cost': 2,
+            'Health Recovered': 4
+        },
+        "Chicago-style Pizza": {
+            'Food Cost': 3,
+            'Health Recovered': 6
+        },
+        "Cheeseburger": {
+            'Food Cost': 4,
+            'Health Recovered': 7
+        },
+        "Philly Cheese Steak": {
+            'Food Cost': 5,
+            'Health Recovered': 10
+        },
+        "Nashville Hot Chicken": {
+            'Food Cost': 8,
+            'Health Recovered': 20
+        },
+        "Melania's Homemade Dinner from 2019": {
+            'Food Cost': 15,
+            'Health Recovered': 35
+        },
+        "Barack's Sandwich from 2015 ": {
+            'Food Cost': 25,
+            'Health Recovered': 100
+        },
     }
 
     if player["Visited Shop"]:
@@ -38,7 +102,10 @@ def shopkeeper(player):
     if initial_selection == 'Leave':
         print("See ya later alligator!")
     elif initial_selection == 'Buy Weapons':
-        get_user_choice('weapons-choice', 'What weapons would you like to buy?', weapons_dictionary.keys())
+        weapons_bought = get_user_choice('weapons-choice', 'What weapons would you like to buy?',
+                                         weapons_dictionary.keys())
+    elif initial_selection == 'Buy Food':
+        food_bought = get_user_choice('food-choice', 'What food would you like to buy?', food_dictionary.keys())
 
 
 def main():
