a5faa4f7329c907096955a25e5832ef1af494cd1
ankitahlwat <ankitahlwat54@gmail.com>
2022-11-27 16:01:14 -0800

    Add special if statement and main

---
+++ game.py
@@ -0,0 +1,17 @@
+"""
+Ankit Ahlwat / A01317232 / ankitahlwat
+
+Jas Randhawa / A01236951 / jasbcit
+"""
+
+
+def game():
+    pass
+
+
+def main():
+    game()
+
+
+if __name__ == '__main__':
+    main()
