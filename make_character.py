d3339203587ab661259f1f616282c5710cdab818
jasbcit <99767793+jasbcit@users.noreply.github.com>
2022-12-01 01:25:58 -0800

    Update make_character.py
    Add Political Party to character_dictionary

--- make_character.py
+++ make_character.py
@@ -1,4 +1,4 @@
-def make_character(name):
+def make_character(name, party):
     character_dictionary = {
         'Name': name,
         'X-coordinate': 0,
@@ -10,6 +10,7 @@ def make_character(name):
         'Attack Points': 1,
         'Visited Shop': False,
         'Items': [],
+        'Political Party': party
     }
     return character_dictionary
 
