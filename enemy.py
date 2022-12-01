f564d903846821033a1a922c9355c3fb8a364337
jasbcit <99767793+jasbcit@users.noreply.github.com>
2022-12-01 01:27:35 -0800

    Add enemy.py
    Create Enemy class

---
+++ enemy.py
@@ -0,0 +1,16 @@
+class Enemy:
+
+    def __init__(self, name: str, enemy_type: str, level: int, hp: int, attack: int):
+        self.name = name
+        self.enemy_type = enemy_type
+        self.level = level
+        self.hp = hp
+        self.attack = attack
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
