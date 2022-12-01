df4b641c3bfb9fccc31745461e0e6328ab15d37a
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-01 10:05:17 -0800

    Update character.json file with new coordinates

--- describe_current_location.py
+++ describe_current_location.py
@@ -17,23 +17,35 @@ def is_valid_move(current_location):
     with open("coordinates.json", "r") as file_object:
         coordinates = json.load(file_object)
         if current_location not in coordinates or coordinates[current_location] == "⬛":
-            print("That is not valid")
+            return False
+        return True
 
-def update_current_location(y_coordinate,x_coordinate, direction):
-    y_map = {"North": -1, "South": +1}
-    x_map = {"West": -1, "East": +1}
-    if direction in y_map:
+
+def update_current_location(y_coordinate, x_coordinate, direction):
+    y_map = {"North": -1, "South": +1, "West": 0, "East": 0}
+    x_map = {"West": -1, "East": +1, "North": 0, "South": 0}
+    if is_valid_move(f'{y_coordinate + y_map[direction]}:{x_coordinate + x_map[direction]}'):
         y_coordinate += y_map[direction]
-    if direction in x_map:
         x_coordinate += x_map[direction]
-    is_valid_move(f'{y_coordinate}:{x_coordinate}')
+        with open("character.json", "r") as file_object:
+            character_dictionary = json.load(file_object)
+            character_dictionary["Y-coordinate"] = y_coordinate
+            character_dictionary["X-coordinate"] = x_coordinate
+        with open("character.json", "w") as file_object:
+            json.dump(character_dictionary, file_object)
+
+    else:
+        print("Tru again")
+
 
 def describe_current_location(y_coordinate, x_coordinate):
     with open("white_house_room_descriptions.json", "r") as file_object:
         room_description_dictionary = json.load(file_object)
         print(room_description_dictionary[f'{y_coordinate}:{x_coordinate}'])
     direction = get_user_choice("Decision", "Where would you like to go", ["North", "East", "West", "South"])
-    update_current_location(y_coordinate,x_coordinate, direction)
+    update_current_location(y_coordinate, x_coordinate, direction)
+
+
 def find_current_location():
     print("Current location")
     with open("character.json", "r") as file_object:
