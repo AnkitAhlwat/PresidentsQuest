c4eaa48d9d79c1e32f628b4384881be2e906edbc
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-02 12:48:17 -0800

    Add check_for_random_enemy() and debug visuals

--- describe_current_location.py
+++ describe_current_location.py
@@ -1,17 +1,36 @@
 import json
-import os
+from combat import setup_combat
 from get_user_choice import get_user_choice
+from die import Die
 
 
-def display_map(coordinates):
+def display_map():
+    with open("coordinates.json") as file_object:
+        coordinates = json.load(file_object)
     print("Current map")
+
+    with open("character.json","r") as file_object:
+        character_dictionary = json.load(file_object)
+        if character_dictionary["Icons"] == "cmd":
+            end_string = ""
+        else:
+            end_string = "`"
+
     position = 0
     for room, icons in coordinates.items():
-        print(icons, end="`")
+        print(icons, end=end_string)
         position += 1
         if position == 8:
             print()
             position = 0
+
+def check_for_random_enemy():
+    current_die = Die(10)
+    dice_roll = current_die.roll_die()
+    if dice_roll <= 2:
+        print("A random enemy politician!")
+        setup_combat()
+        find_current_location()
 
 
 def is_valid_move(current_location):
@@ -21,19 +40,24 @@ def is_valid_move(current_location):
             return False
         return True
 
-
 def update_current_location(y_coordinate, x_coordinate, direction):
     y_map = {"North": -1, "South": +1, "West": 0, "East": 0}
     x_map = {"West": -1, "East": +1, "North": 0, "South": 0}
+    with open("coordinates.json", "r") as file_object:
+        coordinates = json.load(file_object)
     if is_valid_move(f'{y_coordinate + y_map[direction]}:{x_coordinate + x_map[direction]}'):
         y_coordinate += y_map[direction]
         x_coordinate += x_map[direction]
         with open("character.json", "r") as file_object:
             character_dictionary = json.load(file_object)
+            coordinates[f'{character_dictionary["Y-coordinate"]}:{character_dictionary["X-coordinate"]}'] = "[ ]"
             character_dictionary["Y-coordinate"] = y_coordinate
             character_dictionary["X-coordinate"] = x_coordinate
+            coordinates[f'{character_dictionary["Y-coordinate"]}:{character_dictionary["X-coordinate"]}'] = "[X]"
         with open("character.json", "w") as file_object:
             json.dump(character_dictionary, file_object)
+        with open("coordinates.json", "w") as file_object:
+            json.dump(coordinates, file_object)
         describe_current_location(y_coordinate, x_coordinate)
 
     else:
@@ -42,10 +66,11 @@ def update_current_location(y_coordinate, x_coordinate, direction):
 
 
 def describe_current_location(y_coordinate, x_coordinate):
-    os.system("cls")
+    display_map()
     with open("white_house_room_descriptions.json", "r") as file_object:
         room_description_dictionary = json.load(file_object)
         print(room_description_dictionary[f'{y_coordinate}:{x_coordinate}'])
+    check_for_random_enemy()
     direction = get_user_choice("Decision", "Where would you like to go", ["North", "East", "West", "South"])
     update_current_location(y_coordinate, x_coordinate, direction)
 
@@ -58,16 +83,8 @@ def find_current_location():
         y_coordinate = character_dictionary["Y-coordinate"]
         describe_current_location(y_coordinate, x_coordinate)
 
-    # for coordinate, room in coordinates.items():
-    #     if room == "🤵":
-    #         current_location = coordinate
-    #         describe_current_location(current_location)
-
 
 def run():
-    with open("coordinates.json") as file_object:
-        coordinates = json.load(file_object)
-    display_map(coordinates)
     find_current_location()
 
 
