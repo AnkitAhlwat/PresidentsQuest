7ef5ba0a878a95c5b5a14c75b74855ab7aed60bc
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-02 12:48:27 -0800

    Add die class

---
+++ die.py
@@ -0,0 +1,19 @@
+import random
+
+
+class Die:
+
+    def __init__(self, number_of_sides):
+        self.face_value = 1
+        self.number_of_sides = number_of_sides
+
+    def get_face_value(self):
+        return self.face_value
+
+    def set_size(self, new_size):
+        if new_size > 0:
+            self.number_of_sides = new_size
+
+    def roll_die(self):
+        self.face_value = random.randint(1, self.number_of_sides)
+        return self.face_value
