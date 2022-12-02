29130247fe1beff6946fc25d84d674ebbf4b663f
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-02 12:47:01 -0800

    Add setting option for command prompt icons

--- make_board.py
+++ make_board.py
@@ -1,6 +1,6 @@
 import json
 def make_board():
-    map_icons = {
+    visual_icons = {
         "0": "\U00002B1B",
         "1": "\U00002B1C",
         "S": "\U0001F6D2",
@@ -8,21 +8,29 @@ def make_board():
         "C": "\U0001F935"
 
     }
+    cmd_icons = {
+        "0": "   ",
+        "1": "[ ]",
+        "S": "[S]",
+        "B": "[B]",
+        "C": "[X]"
+    }
     with open("white_house.txt", 'r') as white_house_map:
         coordinates = {}
         board = white_house_map.readlines()
+        with open("character.json", "r") as file_object:
+            character_dictionary = json.load(file_object)
+            icons = character_dictionary['Icons']
+            if icons == "cmd":
+                map_icons = cmd_icons
+            else:
+                map_icons = visual_icons
         for index_row, row in enumerate(board):
-            # print("\t", end="")
             for index_room, room in enumerate(row):
-                # if room in map_icons:
-                #     print(f'{map_icons[room]}', end="")
                 if room != "\n":
                     coordinates[f'{index_row}:{index_room}'] = map_icons.get(room)
-        #     print()
-        # print(coordinates)
         with open("coordinates.json", "w") as file_object:
             json.dump(coordinates, file_object)
-
 
 def main():
     make_board()
