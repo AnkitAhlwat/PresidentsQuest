3a8b04e40bb43aaa36132203dff894eaf0a90aa8
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-02 19:18:00 -0800

    Polish file for readability

--- game.py
+++ game.py
@@ -8,10 +8,9 @@ from make_board import make_board
 from make_character import make_character
 from describe_current_location import *
 def game():
-    board = make_board()
-    character = make_character("Player Name", "Republican")
+    make_board()
+    make_character("Player Name", "Republican")
     achieved_goal = False
-    escape = False
     while not achieved_goal:
         setup_current_location()
         # if character_has_leveled():
