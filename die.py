a2dfbe6af36b36b2f929872684efca524f35976b
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-02 22:49:55 -0800

    Add docstrings to Die Class

--- die.py
+++ die.py
@@ -2,18 +2,35 @@ import random
 
 
 class Die:
-
+    """Represent a die with the number of sides."""
     def __init__(self, number_of_sides):
+        """Create a Die object """
         self.face_value = 1
         self.number_of_sides = number_of_sides
 
     def get_face_value(self):
+        """Print the face value"""
         print(self.face_value)
 
     def set_size(self, new_size):
+        """Set the new size of the die"""
         if new_size > 0:
             self.number_of_sides = new_size
 
     def roll_die(self):
+        """Roll the die and print the face value"""
         self.face_value = random.randint(1, self.number_of_sides)
         return self.get_face_value()
+
+
+def main():
+    """
+    Drive the example.
+    """
+    die = Die(6)
+    for _ in range(20):
+        die.roll_die()
+
+
+if __name__ == '__main__':
+    main()
