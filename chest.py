8ea782606f524ee82a6da6a9a23dd9779aae7468
ankitahlwat <ankitahlwat54@gmail.com>
2022-12-03 20:53:54 -0800

    Debug chest do disappear after clicked and give xp

--- chest.py
+++ chest.py
@@ -1,9 +1,10 @@
 import json
 
+import run_game
 from die import Die
 from time import sleep
-from combat import check_if_level_up
-def open_loot():
+import combat
+def open_loot(y_coordinate,x_coordinate):
     with open("character.json", "r") as file_object:
         player = json.load(file_object)
     loot_die = Die(10)
@@ -12,8 +13,17 @@ def open_loot():
     print("Oh looks like I found a chest...")
     sleep(2)
     print(f'Congratulations, you received {xp_result} XP and {gold_result} gold!')
-    check_if_level_up(player)
-
+    player["XP"] +=xp_result
+    player["Money"] += gold_result
+    combat.check_if_level_up(player)
+    with open("character.json", "w") as file_object:
+        json.dump(player, file_object)
+    with open("coordinates.json", "r") as file_object:
+        coordinates_map = json.load(file_object)
+    coordinates_map[f'{y_coordinate}:{x_coordinate}'] = "[ ]"
+    with open("coordinates.json", "w") as file_object:
+        json.dump(coordinates_map, file_object)
+    run_game.setup_current_location()
 def main():
     open_loot()
 
